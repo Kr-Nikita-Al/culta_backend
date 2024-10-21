f286c6a7e1b05a28d4a747b17ecc7d52e0d5e597
Kr-Nikita-Al <nikitakropotov2013@gmail.com>
2024-10-21 16:04:06 +0300

    Add all fields for product_card model

--- product_card/interface_request/create_product_card_request.py
+++ product_card/interface_request/create_product_card_request.py
@@ -6,34 +6,34 @@ from pydantic import BaseModel, constr, conint, StrictBool
 class CreateProductCardRequest(BaseModel):
     company_id: str
     title: Optional[constr(min_length=1, max_length=20)]
-    # sub_title: constr(min_length=1, max_length=50)
-    # header: constr()
-    # description: constr()
-    # hint_header: constr()
-    # hint_description: constr()
-    # product_category: constr()
-    # custom_product_category: constr()
-    # product_release_type: constr()
-    # allergens_list: constr()
-    # quantity_system: constr()
-    # tags: constr()
-    # count_number: conint()
-    # price_field_1: conint()
-    # price_field_2: conint()
-    # cost_price_field_1: conint()
-    # cost_price_field_2: conint()
-    # cashback_field_1: conint()
-    # cashback_field_2: conint()
-    # product_quantity: conint()
-    # calorie_content: conint()
-    # proteins: conint()
-    # fats: conint()
-    # carbohydrates: conint()
-    # cooking_time: conint()
-    # bonuses_payment: StrictBool
-    # single_product_type: StrictBool
-    # is_sharpness: StrictBool
-    # is_hotness: StrictBool
-    # company_group_id: conint()
-    # product_image_id: constr()
-    # icon_image_id: constr()
+    sub_title: Optional[constr(min_length=1, max_length=50)]
+    header: Optional[constr()]
+    description: Optional[constr()]
+    hint_header: Optional[constr()]
+    hint_description: Optional[constr()]
+    product_category: Optional[constr()]
+    custom_product_category: Optional[constr()]
+    product_release_type: Optional[constr()]
+    allergens_list: Optional[constr()]
+    quantity_system: Optional[constr()]
+    tags: Optional[constr()]
+    count_number: Optional[conint()]
+    price_field_1: Optional[conint()]
+    price_field_2: Optional[conint()]
+    cost_price_field_1: Optional[conint()]
+    cost_price_field_2: Optional[conint()]
+    cashback_field_1: Optional[conint()]
+    cashback_field_2: Optional[conint()]
+    product_quantity: Optional[conint()]
+    calorie_content: Optional[conint()]
+    proteins: Optional[conint()]
+    fats: Optional[conint()]
+    carbohydrates: Optional[conint()]
+    cooking_time: Optional[conint()]
+    bonuses_payment: Optional[StrictBool]
+    single_product_type: Optional[StrictBool]
+    is_sharpness: Optional[StrictBool]
+    is_hotness: Optional[StrictBool]
+    company_group_id: Optional[conint()]
+    product_image_id: Optional[constr()]
+    icon_image_id: Optional[constr()]
