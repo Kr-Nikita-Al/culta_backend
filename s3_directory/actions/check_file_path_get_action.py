c98f745d980a10d311588999bb4ddec51037f534
Kr-Nikita-Al <nikitakropotov2013@gmail.com>
2025-02-12 19:23:27 +0300

    fix req.txt 3

--- s3_directory/actions/check_file_path_get_action.py
+++ s3_directory/actions/check_file_path_get_action.py
@@ -12,6 +12,7 @@ def check_file_path_get(obj_dict: Dict, file_path: str, company_id: UUID) -> boo
    :return: возможность выгрузить файл
    """
     if company_id != EMPTY_UUID:
-        real_dir_path = BASE_STORAGE_DIRECTORY.COMPANY + "company_{0}".format(str(company_id)) + '/'
-        return real_dir_path == file_path and file_path in obj_dict.keys()
+        real_dir_path = BASE_STORAGE_DIRECTORY.COMPANY + "company_{0}".format(str(company_id))
+        request_dir_path = file_path[:file_path.index('/', file_path.index('/') + 1)]
+        return real_dir_path == request_dir_path and file_path in obj_dict.keys()
     return file_path in obj_dict.keys()
